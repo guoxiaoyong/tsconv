727c25d428b497dc144bfefe7cb329047bb296f9
Xiaoyong Guo <xguo@prestolabs.io>
2019-08-31 09:54:15 +0800

    update

--- tsconv/__init__.py
+++ tsconv/__init__.py
@@ -1,10 +1,11 @@
-import enum
-import time
 import datetime
-import pytz
+import enum
+import os
 import sys
+import time
 
-SHANGHAI_TZ = pytz.timezone('Asia/Shanghai')
+import pytz
+
 
 MIN_DATE = datetime.datetime(year=2000, month=1, day=1)
 MAX_DATE = MIN_DATE + datetime.timedelta(days=100*365)
@@ -27,12 +28,16 @@ def check_interval():
     MAX_TS  < MIN_TS * UnitPerSec.MILI_SEC
 
 
+class InvalidTsValue(ValueError):
+  pass
+
+
 def guess_ts_unit(ts: float):
   for unit_per_sec in UnitPerSec:
     if is_reasonable(ts/unit_per_sec.value):
       return unit_per_sec
   else:
-    raise ValueError('Timestamp value not in reasonable range!')
+    raise InvalidTsValue('Timestamp value not in reasonable range!')
 
 
 def ts_to_datetime(ts, tz):
@@ -46,14 +51,20 @@ def print_usage():
 
 
 def main(argv=None):
-  check_interval()
   argv = argv or sys.argv
-  if len(argv) >= 2:
+  check_interval()
+
+  tz_str = os.environ.get('TZ', 'UTC')
+  tzinfo = pytz.timezone(tz_str)
+  try:
     timestamp = float(argv[1])
-    dt = ts_to_datetime(timestamp, SHANGHAI_TZ)
-    print(dt)
-  else:
+    result = ts_to_datetime(timestamp, tzinfo)
+    print(result)
+  except InvalidTsValue:
+    print(f'Reasonable time range [{MIN_DT}, {MAX_DT}]')
+  except (IndexError, KeyError):
     print_usage()
+
 
 if __name__ == "__main__":
   sys.exit(main(sys.argv))
