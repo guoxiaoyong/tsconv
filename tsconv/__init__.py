95707fcfdad004f9d3f2c856a9c375dc7cbe7b1f
Xiaoyong Guo <xguo@prestolabs.io>
2019-09-14 22:53:45 +0800

    update

--- tsconv/__init__.py
+++ tsconv/__init__.py
@@ -1,10 +1,9 @@
 import datetime
 import enum
 import os
+import pytz
 import sys
 import time
-
-import pytz
 
 
 MIN_DATE = datetime.datetime(year=2000, month=1, day=1)
@@ -16,9 +15,16 @@ VERSION = '0.0.3'
 
 class UnitPerSec(enum.IntEnum):
     SEC = 1
-    MILI_SEC = int(1e3)
+    MILLI_SEC = int(1e3)
     MICRO_SEC = int(1e6)
     NANO_SEC = int(1e9)
+
+
+class UnitName(enum.Enum):
+    SEC = 'second'
+    MILLI_SEC = 'millisecond'
+    MICRO_SEC = 'microsecond'
+    NANO_SEC = 'nanosecond'
 
 
 def is_reasonable(ts: float):
@@ -26,49 +32,54 @@ def is_reasonable(ts: float):
 
 
 def check_interval():
-    MAX_TS  < MIN_TS * UnitPerSec.MILI_SEC
+    MAX_TS  < MIN_TS * UnitPerSec.MILLI_SEC
 
 
 class InvalidTsValue(ValueError):
-  pass
+    pass
 
 
 def guess_ts_unit(ts: float):
-  for unit_per_sec in UnitPerSec:
-    if is_reasonable(ts/unit_per_sec.value):
-      return unit_per_sec
-  else:
+    for unit_per_sec in UnitPerSec:
+        if is_reasonable(ts/unit_per_sec.value):
+            return unit_per_sec
     raise InvalidTsValue('Timestamp value not in reasonable range!')
 
 
 def ts_to_datetime(ts, tz):
-  unit_per_sec = guess_ts_unit(ts)
-  ts = ts / unit_per_sec.value
-  return datetime.datetime.fromtimestamp(ts, tz)
+    unit_per_sec = guess_ts_unit(ts)
+    ts = ts / unit_per_sec.value
+    dt = datetime.datetime.fromtimestamp(ts, tz)
+    return {
+        'datetime': dt,
+        'seconds': ts,
+        'nanoseconds': int(ts * 1e9),
+        'tz': tz,
+    }
 
 
 def print_usage():
-  program_name = sys.argv[0]
-  print(f'{program_name} {VERSION}')
-  print('usage:')
-  print(f'\t{program_name} timestamp')
+    program_name = sys.argv[0]
+    print(f'{program_name} {VERSION}')
+    print('usage:')
+    print(f'\t{program_name} timestamp')
 
 
 def main(argv=None):
-  argv = argv or sys.argv
-  check_interval()
+    argv = argv or sys.argv
+    check_interval()
+    tz_str = os.environ.get('TZ', 'UTC')
+    tzinfo = pytz.timezone(tz_str)
 
-  tz_str = os.environ.get('TZ', 'UTC')
-  tzinfo = pytz.timezone(tz_str)
-  try:
-    timestamp = float(argv[1])
-    result = ts_to_datetime(timestamp, tzinfo)
-    print(result)
-  except InvalidTsValue:
-    print(f'Reasonable time range [{MIN_DATE}, {MAX_DATE}]')
-  except (IndexError, KeyError):
-    print_usage()
+    try:
+        timestamp = float(argv[1])
+        result = ts_to_datetime(timestamp, tzinfo)
+        print(result)
+    except InvalidTsValue:
+        print(f'Reasonable time range [{MIN_DATE}, {MAX_DATE}]')
+    except (IndexError, KeyError):
+        print_usage()
 
 
 if __name__ == "__main__":
-  sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
